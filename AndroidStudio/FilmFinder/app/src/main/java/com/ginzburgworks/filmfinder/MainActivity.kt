ec304753895dd8ec8e66cbe7ebd50c9c27b21506
ilginzburg <ilya.ginsburg@gmail.com>
2021-08-02 16:01:00 +0300

    module_21 night theme ver001

--- AndroidStudio/FilmFinder/app/src/main/java/com/ginzburgworks/filmfinder/MainActivity.kt
+++ AndroidStudio/FilmFinder/app/src/main/java/com/ginzburgworks/filmfinder/MainActivity.kt
@@ -2,7 +2,6 @@ package com.ginzburgworks.filmfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,9 +9,44 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initButtons()
+      //  initButtons()
+        initNavigation()
+
+
     }
 
+    private fun initNavigation() {
+        topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+        bottom_navigation.setOnNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+
+    /*
     private fun initButtons() {
         button_menu.setOnClickListener() {
             Toast.makeText(this, getString((R.string.btn_menu)), Toast.LENGTH_SHORT).show()
@@ -24,10 +58,12 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, getString((R.string.btn_watch_later)), Toast.LENGTH_SHORT).show()
         }
         button_properties.setOnClickListener() {
-            Toast.makeText(this, getString((R.string.btn_properties)), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString((R.string.btn_selections)), Toast.LENGTH_SHORT).show()
         }
         button_settings.setOnClickListener() {
             Toast.makeText(this, getString((R.string.btn_settings)), Toast.LENGTH_SHORT).show()
         }
     }
+    */
+
 }
